db0765d9d29dd2952ec7dc6d7529233062b412fd
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-01-10 15:38:56 +0200

    create AddItemForm story

---
+++ src/stories/AddItemForm.stories.tsx
@@ -0,0 +1,73 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+
+import {AddItemForm} from "../AddItemForm";
+import {action} from "@storybook/addon-actions";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Todolist/AddItemForm',
+    component: AddItemForm,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        addItem: {
+            description: 'Button clicked inside form',
+        },
+    },
+} as ComponentMeta<typeof AddItemForm>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof AddItemForm> = (args) => <AddItemForm {...args} />;
+
+export const AddItemFormStory = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+AddItemFormStory.args = {
+    addItem: action('Button clicked')
+};
+
+const TemplateWithError: ComponentStory<typeof AddItemForm> = (args) => {
+    let [title, setTitle] = useState("")
+    let [error, setError] = useState<string | null>("Title is required")
+
+    const addItem = () => {
+        if (title.trim() !== "") {
+            args.addItem(title);
+            setTitle("");
+        } else {
+            setError("Title is required");
+        }
+    }
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (error) {
+            setError(null);
+        }
+        if (e.charCode === 13) {
+            addItem();
+        }
+    }
+
+    return <div>
+        <TextField
+            value={title}
+            onChange={onChangeHandler}
+            onKeyPress={onKeyPressHandler}
+            id="outlined-basic"
+            label={error ? "Title is required" : "type out here..."}
+            variant="outlined"
+            size="small"
+            error={!!error}
+        />
+
+        <Button variant="contained" style={{maxWidth: '38px', maxHeight: '38px', minWidth: '38px', minHeight: '38px'}}
+                onClick={addItem}>+</Button>
+    </div>
+};
+
+export const AddItemFormWithErrorStory = TemplateWithError.bind({});
