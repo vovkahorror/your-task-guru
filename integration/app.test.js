a5ad4b827b63e7abe1005737a1628e184dcf1371
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-01-20 19:27:19 +0200

    upgrade Storybook

--- integration/app.test.js
+++ integration/app.test.js
@@ -1,7 +1,7 @@
 describe('addItemForm', () => {
     it('base example, visually looks correct', async () => {
         // APIs from jest-puppeteer
-        await page.goto('http://localhost:9009/iframe.html?args=&id=todolist-additemform--add-item-form-story&viewMode=story')
+        await page.goto('http://localhost:9009/iframe.html?id=todolist-additemform--add-item-form-story&viewMode=story')
         const image = await page.screenshot()
 
         // API from jest-image-snapshot
