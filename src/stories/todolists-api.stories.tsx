661306aad46ed3103827d2aefdc57e2092e84fe2
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-01-23 02:09:09 +0200

    create CreateTodolist story

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -7,6 +7,9 @@ export default {
 
 const settings = {
     withCredentials: true,
+    headers: {
+        'api-key': 'd09df4a6-624c-4f2c-a2ad-d9a381941271',
+    },
 };
 
 export const GetTodolists = () => {
@@ -22,6 +25,8 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null);
     useEffect(() => {
+        axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {title: 'new todolist'}, settings)
+            .then(res => setState(res.data));
     }, []);
 
     return <div>{JSON.stringify(state)}</div>;
