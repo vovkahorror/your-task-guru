7e04834648bd0839916205c4ae1675986800082c
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-01-24 02:59:58 +0200

    create UpdateTodolistTitle story

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -33,14 +33,22 @@ export const CreateTodolist = () => {
 };
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null);
+    const todolistID = 'e08d95d9-3ada-4223-9f0c-cbd223d98f39'
+
     useEffect(() => {
+        axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistID}`, settings)
+            .then(res => setState(res.data));
     }, []);
 
     return <div>{JSON.stringify(state)}</div>;
 };
 export const UpdateTodolistTitle = () => {
     const [state, setState] = useState<any>(null);
+    const todolistID = '6e204e03-8983-4195-957a-9584546f3b31'
+
     useEffect(() => {
+        axios.put(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistID}`, {title: 'NEW TITLE'}, settings)
+            .then(res => setState(res.data));
     }, []);
 
     return <div>{JSON.stringify(state)}</div>;
