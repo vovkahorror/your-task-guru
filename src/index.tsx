984ddc8816466f758bcd5574b3dbaaaa1077b34d
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-07-02 01:14:14 +0300

    feat: renamed the project

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,16 @@ import {createRoot} from 'react-dom/client';
 import {Provider} from 'react-redux';
 import {store} from './app/store';
 import App from './app/App';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
 );
 
 // If you want your app to work offline and load faster, you can change
