8ad7dff5ce1d2380c0dcc05c76f976d030015184
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-08-07 01:18:32 +0300

    feat: changed app background

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './index.css';
+import './index.scss';
 import * as serviceWorker from './serviceWorker';
 import {createRoot} from 'react-dom/client';
 import {Provider} from 'react-redux';
