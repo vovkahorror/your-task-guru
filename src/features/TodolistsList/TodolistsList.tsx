8e514a228ffe6d28f9ab8036e5c240a27c3ac091
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-01-24 00:45:42 +0200

    fix: autoscroll

--- src/features/TodolistsList/TodolistsList.tsx
+++ src/features/TodolistsList/TodolistsList.tsx
@@ -14,12 +14,7 @@ import {rectSortingStrategy, SortableContext, sortableKeyboardCoordinates} from 
 import {SmartMouseSensor} from '../../common/custom-sensors/SmartMouseSensor';
 import {SmartTouchSensor} from '../../common/custom-sensors/SmartTouchSensor';
 import {useScreenSize} from '../../utils/custom-hooks/useScreenSize';
-import {
-    restrictToFirstScrollableAncestor,
-    restrictToHorizontalAxis,
-    restrictToParentElement,
-    restrictToVerticalAxis, restrictToWindowEdges,
-} from '@dnd-kit/modifiers';
+import {restrictToParentElement} from '@dnd-kit/modifiers';
 
 export const TodolistsList = () => {
     const isLoggedIn = useAppSelector(authSelectors.selectIsLoggedIn);
@@ -85,7 +80,7 @@ export const TodolistsList = () => {
                 collisionDetection={closestCenter}
                 onDragEnd={handleDragEnd}
                 autoScroll={isMobile}
-                modifiers={[restrictToParentElement, restrictToWindowEdges]}
+                modifiers={[restrictToParentElement]}
             >
                 <SortableContext
                     items={todolists}
